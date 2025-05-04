7bbde7ab20c9ab6c3172a23c7761b2df030028fa
Jean Pierre <jeanpierreh11@gmail.com>
2025-05-04 11:30:08 -0500

    Revert "bucle infinito"
    This reverts commit 061677cb5ee6821e5a87be342f87f3c52543897c.

--- new-feature.js
+++ new-feature.js
@@ -1,14 +1,3 @@
 const b = "Holam aqui hay un error";
 
-while (true) {
-  console.log("bucle infitino");
-}
-
-// more changes 
-
-// more changes 
-
-
-// more changes 
-
-console.log(b); 
+console.log(a);
